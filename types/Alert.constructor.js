4cbe06828ff1f42f1c8035f8eb49d73eb98a07b7
Thomas Rory Gummerson <thomas@gummerson.no>
2019-07-31 12:17:35 +0200

    Alert

--- types/Alert.constructor.js
+++ types/Alert.constructor.js
@@ -1,55 +1,60 @@
-<?php
+// Dependencies:
+// Component, html, attr_append, Compiler
+/* global Component, html, attr_append, Compiler */
 
-function Alert($input = "") {
-$base_class = "alert";
-$default = [
-                "content"   =>   "Content placeholder",
-                "tag"       =>   "div",
-                "attr"      =>   "",
-                "template"  =>   "alert-warning fade show",
-                "dismisable"=>   true,
-                "style"     =>   "",
-                "script"    =>   ""
-            ];
-
-foreach(Component($input, $default, $base_class) as $key => $value) {
-     $$key = $value;
+function Alert(input = '') {
+	const {
+		content,
+		tag,
+		attr,
+		template,
+		dismisable,
+		style,
+		script,
+		id
+	} = Component(input, {
+		content: 'Content placeholder',
+		tag: 'div',
+		attr: '',
+		template: 'alert-warning fade show',
+		dismisable: true,
+		style: '',
+		script: ''
+	}, 'alert');
+	return Compiler('alert', [
+		{
+			condition: true,
+			line: html(tag, `id="${id}" class="alert" ${template}` +
+				attr_append(attr, { role: 'alert' }))
+		},
+		{ condition: true, line: content },
+		{
+			condition: dismisable,
+			line: html('button', {
+				type: 'button',
+				class: 'close',
+				'data-dismiss': 'alert',
+				'aria-label': 'Close'
+			}) + html('span', {
+				'aria-hidden': 'true'
+			}) +
+			'&times;' +
+			html('span', '/') +
+			html('button', '/')
+		},
+		{
+			condition: script && script.length > 0,
+			line: html('script') + script + html('script', '/')
+		},
+		{
+			condition: style && style.length > 0,
+			line: html('style') + style + html('style', '/')
+		},
+		{
+			condition: true,
+			line: html(tag, '/')
+		}
+	]);
 }
 
-$scheme =   [
-               [
-                    "condition" => true,
-                    "line"      => html($tag,"id='$id' class='$base_class 
-                    $template' ".attr_append($attr, [
-                         "role" =>  "alert"
-                    ])
-                    )
-               ],
-               [
-                    "condition" => true,
-                    "line"      => $content
-               ],
-               [
-                    "condition" => $dismisable,
-               "line"      => html('button', ['type'=>'button','class'=>'close', 'data-dismiss'=>'alert','aria-label'=>'Close']).
-                                        html('span',['aria-hidden'=>'true']).'&times;'.html('span','/').
-                              html('button', '/')
-               ],
-               [
-                    "condition" => !empty($script),
-                    "line"      => html('script').$script.html('script','/')
-               ],
-               [
-                    "condition" => !empty($style),
-                    "line"      => html('style').$style.html('style','/')
-               ],
-               [
-                    "condition" => true,
-                    "line"      => html($tag,'/')
-               ],
-            ];
-            
-return Compiler($base_class, $scheme);
-}
-
-?>
+export default Alert;
